fb4fb3a5661c67c940e396cacfc1a92d49301724
Dmytro Vyshnevskyi <di593297724ma@gmail.com>
2023-05-12 19:19:20 +0200

    up

--- scripts/main.js
+++ scripts/main.js
@@ -20,20 +20,31 @@ addButton.addEventListener('click', function() {
     todoList.push(newTodo);
     displayMessages();
     localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
 });
 
 function displayMessages(){
     let displayMessage = '';
+    let class_arrea = [];
     todoList.forEach(function(item, i){
         displayMessage += `
-        <li>
-            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-            <laber for='item_${i}'>${item.todo}</laber>
-        </li>
+        <li id='list_${i}'>
+            <div><input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+            <label for='item_${i}'>${item.todo}</label></div>
+            <div><i id='edit_${i}' class="fa-regular fa-pen-to-square"></i>
+            <i id='delete_${i}' class="fa-regular fa-trash-can"></i>
+        </li></div>
         `;
         todo.innerHTML = displayMessage;
+        class_arrea.push(i);
     });
 }
+
+//     let item_delete = document.querySelector('#delete_0');
+//     let list = document.querySelector('#list_0')
+//     item_delete.addEventListener('click', function name(params) {
+//         list.remove();
+// });
 
 todo.addEventListener('change', function(event) {
     let valueLabel = todo.querySelector('[for='+ event.target.getAttribute('id') +']').innerHTML;
